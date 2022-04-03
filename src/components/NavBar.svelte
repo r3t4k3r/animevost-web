3bb4d49a2642728ec3d6837e524a1ea18d489011
r3t4k3r <av.19@list.ru>
2022-04-03 19:10:15 +0300

    добавление вкладки о сайте

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,30 +1,74 @@
 <script>
-    import { FormGroup, Navbar, NavbarBrand, Input } from "sveltestrap";
+    import {
+        Navbar,
+        NavbarBrand,
+        Input,
+        Button,
+        ModalHeader,
+        Modal,
+        ModalBody,
+        ModalFooter,
+        NavItem,
+        NavLink,
+        NavbarToggler,
+        Collapse,
+        Nav,
+    } from "sveltestrap";
     import { uriHeader } from "../config";
     import { navigateTo } from "svelte-router-spa";
 
     let searchInput = "";
 
+    let modalOpen = false;
+    const toggleModal = () => (modalOpen = !modalOpen);
+
+    let navOpen = false;
+    const toggleNav = () => (navOpen = !navOpen);
+
     function handleInput(event) {
         const trimmedText = searchInput.trim();
-        if (trimmedText !== "" && event.key === 'Enter') {
+        if (trimmedText !== "" && event.key === "Enter") {
             searchInput = "";
             navigateTo(`${uriHeader}/search/${trimmedText}`);
         }
     }
+    function handleUpdate(event) {
+        navOpen = event.detail.isOpen;
+    }
 </script>
 
-<Navbar color="dark" dark class="fixed-top">
+<Navbar color="dark" dark class="sticky-top" expand="md">
     <NavbarBrand href={`/${uriHeader}`}>Animevost</NavbarBrand>
-
-    <FormGroup style="margin-bottom: 0!important">
-        <Input
-            type="search"
-            name="search"
-            placeholder="найдите любимое аниме"
-            bind:value={searchInput}
-            on:keydown={handleInput}
-        />
-    </FormGroup>
+    <!-- search -->
+    <NavbarToggler on:click={toggleNav} />
+    <Collapse isOpen={navOpen} navbar expand="md" on:update={handleUpdate}>
+        <Nav class="ms-auto" navbar>
+            <NavItem>
+                <NavLink on:click={toggleModal}>
+                    <!-- about site -->
+                    О сайте
+                </NavLink>
+            </NavItem>
+            <Input
+                type="search"
+                name="search"
+                placeholder="найдите любимое аниме"
+                bind:value={searchInput}
+                on:keydown={handleInput}
+                style="width:15em"
+            />
+        </Nav>
+    </Collapse>
 </Navbar>
-<div style="margin-bottom: 5em" />
+<Modal isOpen={modalOpen} toggle={toggleModal}>
+    <ModalHeader {toggleModal}>О сайте</ModalHeader>
+    <ModalBody>
+        Исходный код: <a href="https://github.com/r3t4k3r/animevost-web">тут</a><br>
+        Спасибо: <a href="https://animevost.org">Animevost</a> и <a href="https://alpha1918.moe/">Альфа 1918</a><br><br>
+        Сбер для пожертований: 5469610011057477
+    </ModalBody>
+    <ModalFooter>
+        <Button color="secondary" on:click={toggleModal}>Закрыть</Button>
+    </ModalFooter>
+</Modal>
+<div style="margin-bottom: 3em" />
