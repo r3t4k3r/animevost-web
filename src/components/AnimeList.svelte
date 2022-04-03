bb6088e20eaac36f48f2df900df8870157240b5a
r3t4k3r <av.19@list.ru>
2022-04-03 19:10:22 +0300

    редизайн

--- src/components/AnimeList.svelte
+++ src/components/AnimeList.svelte
@@ -1,12 +1,5 @@
 <script>
-    import {
-        Accordion,
-        AccordionItem,
-        Row,
-        Col,
-        Image,
-        Badge,
-    } from "sveltestrap";
+    import { Row, Col, Image, Badge, Progress } from "sveltestrap";
     import { scale, fade } from "svelte/transition";
     import Loading from "./Loading.svelte";
     import { Navigate } from "svelte-router-spa";
@@ -16,48 +9,70 @@
 
 {#if animes.length > 0}
     <div class="d-grid gap-5 mb-5 mt-5">
-        {#each animes as anime (anime.id)}
-            <Row>
+        <Row>
+            {#each animes as anime (anime.id)}
                 <Col
-                    class="col-xxl-2 col-xl-3 col-lg-3 col-md-4 col-sm-5 col-12 d-flex justify-content-center"
+                    class="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12"
                 >
-                    <Navigate to={`${uriHeader}/anime/${anime.id}`}>
-                        <div in:scale>
-                            <Image
-                                src={anime.urlImagePreview}
-                                style="height: 300px; width: 200px; border-radius: .25rem"
-                            />
-                        </div>
-                    </Navigate>
-                </Col>
-                <Col
-                    class="col-xxl-10 col-xl-9 col-lg-9 col-md-8 col-sm-7 col-12"
-                >
-                    <Navigate to={`${uriHeader}/anime/${anime.id}`}>
-                        <h4 in:scale>{anime.title}</h4>
-                    </Navigate>
-                    <div in:fade>
-                        <Accordion flush stayOpen>
-                            <AccordionItem header="Год выпуска">
-                                {anime.year}
-                            </AccordionItem>
-                            <AccordionItem header="Жанр">
+                    <Row>
+                        <Col
+                            class="col-xxl-4 col-xl-4 col-lg-5 col-md-4 col-sm-5 col-12 d-flex justify-content-center mb-5"
+                        >
+                            <Navigate to={`${uriHeader}/anime/${anime.id}`}>
+                                <div in:scale>
+                                    <Image
+                                        src={anime.urlImagePreview}
+                                        style="height: 300px; width: 200px; border-radius: .25rem"
+                                    />
+                                </div>
+                                <div in:fade>
+                                    <Progress multi>
+                                        <Progress
+                                            animated
+                                            bar
+                                            color="success"
+                                            value={(anime.rating /anime.votes) * 20}
+                                        >
+                                            {(
+                                                anime.rating / anime.votes
+                                            ).toFixed(2)}
+                                        </Progress>
+                                        <Progress
+                                            striped
+                                            bar
+                                            color="danger"
+                                            value={101 -
+                                                (anime.rating / anime.votes) *
+                                                    20}
+                                        />
+                                    </Progress>
+                                </div>
+                            </Navigate>
+                        </Col>
+                        <Col
+                            class="col-xxl-8 col-xl-8 col-lg-7 col-md-8 col-sm-7 col-12"
+                        >
+                            <Navigate to={`${uriHeader}/anime/${anime.id}`}>
+                                <h4 in:scale>{anime.title.split("/")[0]}</h4>
+                            </Navigate>
+                            <div in:fade>
                                 <h5>
+                                    {anime.type} / {anime.year} /
                                     {#each anime.genre.split(",") as gen}
                                         <Badge class="me-2 mb-1 mt-1" primary>
                                             {gen.trim()}
                                         </Badge>
                                     {/each}
                                 </h5>
-                            </AccordionItem>
-                            <AccordionItem header="Описание">
-                                {@html anime.description}
-                            </AccordionItem>
-                        </Accordion>
-                    </div>
+                                <h5>
+                                    {anime.title.split("[")[1].split("]")[0]}
+                                </h5>
+                            </div>
+                        </Col>
+                    </Row>
                 </Col>
-            </Row>
-        {/each}
+            {/each}
+        </Row>
     </div>
 {:else}
     <Loading />
