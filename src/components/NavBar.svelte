e258f9f008189c4d600c12071cfbd23a51363149
r3t4k3r <av.19@list.ru>
2022-04-22 21:51:08 +0300

    анимедевочка в "О сайте"

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -14,7 +14,7 @@
         Collapse,
         Nav,
     } from "sveltestrap";
-    import { uriHeader } from "../config";
+    import { uriHeader, uriHeaderWD } from "../config";
     import { navigateTo } from "svelte-router-spa";
 
     let searchInput = "";
@@ -44,10 +44,10 @@
     <Collapse isOpen={navOpen} navbar expand="md" on:update={handleUpdate}>
         <Nav class="ms-auto" navbar>
             <NavItem>
-                <NavLink on:click={toggleModal}>
-                    <!-- about site -->
-                    О сайте
-                </NavLink>
+                <NavLink on:click={toggleModal}>О сайте</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink href={`${uriHeader}/schedule`}>Расписание</NavLink>
             </NavItem>
             <Input
                 type="search"
@@ -63,8 +63,21 @@
 <Modal isOpen={modalOpen} toggle={toggleModal}>
     <ModalHeader {toggleModal}>О сайте</ModalHeader>
     <ModalBody>
-        Исходный код: <a target="_blank" href="https://github.com/r3t4k3r/animevost-web">тут</a><br>
-        Спасибо: <a target="_blank" href="https://animevost.org">Animevost</a> и <a target="_blank" href="https://alpha1918.moe/">Альфа 1918</a><br><br>
+        Исходный код: <a
+            target="_blank"
+            href="https://github.com/r3t4k3r/animevost-web">тут</a
+        ><br />
+        Спасибо: <a target="_blank" href="https://animevost.org">Animevost</a> и
+        <a target="_blank" href="https://alpha1918.moe/">Альфа 1918</a><br /><br
+        />
+        <div class="d-flex justify-content-center">
+            <img
+                alt="logo"
+                src={`${uriHeaderWD}/static/img/loading.png`}
+                style="height: 300px; width: 300px; border-radius: .25rem;"
+            />
+        </div>
+        <br />
         Сбер для донатов: 5469610011057477
     </ModalBody>
     <ModalFooter>
