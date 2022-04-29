45894a9a0fa632ace52818e6dfdd9a1d9eeba4a8
r3t4k3r <av.19@list.ru>
2022-04-29 11:49:55 +0300

    фикс отображения расписания с уборими картинками

--- src/Pages/SchedulePage.svelte
+++ src/Pages/SchedulePage.svelte
@@ -65,48 +65,54 @@
 
     function getNameByNumber(number) {
         switch (number) {
-            case 0: return "Понедельник"
-            case 1: return "Втоник"
-            case 2: return "Среда"
-            case 3: return "Четверг"
-            case 4: return "Пятница"
-            case 5: return "Суббота"
-            case 6: return "Воскресенье"
+            case 0:
+                return "Понедельник";
+            case 1:
+                return "Втоник";
+            case 2:
+                return "Среда";
+            case 3:
+                return "Четверг";
+            case 4:
+                return "Пятница";
+            case 5:
+                return "Суббота";
+            case 6:
+                return "Воскресенье";
         }
     }
-
 </script>
 
 {#if isLoaded}
     <h2 class="mb-5">Расписание</h2>
     <Accordion flush stayOpen class="mt-3">
-        {#each [0,1,2,3,4,5,6] as number (number)}
-        <AccordionItem active={new Date().getDay() === (number === 6 ? 0 :number+1)} header="{getNameByNumber(number)}">
-            <Row>
-                
-                {#each getAnimesByDay(number) as anime (anime.id)}
-                    <Col
-                        class="col-xxl-2 col-xl-2 col-lg-2 col-md-3 col-sm-4 col-6"
-                    >
-                    <Navigate to={`${uriHeader}/anime/${anime.id}`}>
-                            <div class="d-flex justify-content-center" style="width: 140px;">
+        {#each [0, 1, 2, 3, 4, 5, 6] as number (number)}
+            <AccordionItem
+                active={new Date().getDay() === (number === 6 ? 0 : number + 1)}
+                header={getNameByNumber(number)}
+            >
+                <Row>
+                    {#each getAnimesByDay(number) as anime (anime.id)}
+                        <Col
+                            class="col-xxl-2 col-xl-2 col-lg-2 col-md-3 col-sm-4 col-6"
+                        >
+                            <Navigate to={`${uriHeader}/anime/${anime.id}`}>
                                 <img
                                     alt="logo"
                                     src={anime.urlImagePreview}
-                                    style="height: 200px; width: 140px; border-radius: .25rem;"
+                                    style="width: 100%; border-radius: .25rem;"
                                     on:error={(obj) => {
                                         obj.target.src = `${uriHeaderWD}/static/img/alpha.png`;
                                     }}
                                 />
-                            </div>
-                            <div class="mb-3">
-                                {anime.name}
-                            </div>
-                    </Navigate>
-                    </Col>
-                {/each}
-            </Row>
-        </AccordionItem>
+                                <div class="mb-3">
+                                    {anime.name}
+                                </div>
+                            </Navigate>
+                        </Col>
+                    {/each}
+                </Row>
+            </AccordionItem>
         {/each}
     </Accordion>
 {:else}
