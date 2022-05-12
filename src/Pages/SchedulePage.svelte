66673557c34b69f82cd6d18d660352e0845fd321
r3t4k3r <av.19@list.ru>
2022-05-12 18:03:19 +0300

    недофикс расписания

--- src/Pages/SchedulePage.svelte
+++ src/Pages/SchedulePage.svelte
@@ -22,6 +22,7 @@
             mode: "cors", // no-cors, *cors, same-origin
             cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
             credentials: "same-origin", // include, *same-origin, omit
+            referrerPolicy: "no-referrer"
         });
         const json = await response.json();
         return json;
