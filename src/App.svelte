9c03330e4b4a43c2a34fcaa8e870233eb6a01b4e
Петя Иванов <av.19@list.ru>
2022-06-12 23:06:54 +0300

    удаление коментариев от фона

--- src/App.svelte
+++ src/App.svelte
@@ -3,24 +3,10 @@
 	import NavBar from "./components/NavBar.svelte";
 	import { Router } from "svelte-router-spa";
 	import { routes } from "./routes";
-	import {uriHeaderWD} from "./config";
-	// const background_url = `${uriHeaderWD}/static/img/background.png`;
 </script>
-<!-- <div class="background" style="background-image: url({background_url});">
-</div> -->
-<NavBar/>
 
-	<Container>
-		<Router {routes} />
-	</Container>
+<NavBar />
 
-<!-- <style>
-	.background {
-		position: fixed; /* Фиксируем на одном месте */
-    	left: 0; right: 0; /* Вся ширин */
-    	top: 0; bottom: 0; /* Вся высота */
-    	z-index: -1; 
-		background-attachment: fixed;
-		filter: blur(4px);
-	}
-</style> -->
+<Container>
+	<Router {routes} />
+</Container>
