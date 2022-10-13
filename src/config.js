7ff02378985df8c1f1f96ca8fb56caa57f23433e
r3t4k3r <av.19@list.ru>
2022-10-13 21:59:51 +0300

    add cors bypass, and fuck animevost

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 const uriHeader = "" //"animevost-web" or "" for localhost
 const uriHeaderWD = uriHeader === "" ? '' : `/${uriHeader}`
-const apiUrl = "https://api.animetop.info/v1" // https://api.animevost.org/v1
+const apiUrl = "https://fuck-anixart.herokuapp.com/https://api.animetop.info/v1" // https://api.animevost.org/v1
 export { uriHeader, uriHeaderWD, apiUrl }
