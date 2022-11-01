a89d2b1e76b9d81a1004c575ed7d058dcd828fa1
r3t4k3r <av.19@list.ru>
2022-11-02 01:33:56 +0300

    распознование среды исполнениня кода

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
-const uriHeader = "" //"animevost-web" or "" for localhost
+const localHostnames = ["localhost", "127.0.0.1", "0.0.0.0"] // список адресов для которых роутинг будет из корня
+const myPathToPage = "animevost-web" // корневой юрл если адреса не было в списке выше
+
+const uriHeader = localHostnames.includes(location.hostname) ? "" : myPathToPage // "animevost-web" for prod or "" for localhost
 const uriHeaderWD = uriHeader === "" ? '' : `/${uriHeader}`
 const apiUrl = "https://fuck-anixart.herokuapp.com/https://api.animetop.info/v1" // https://api.animevost.org/v1
 export { uriHeader, uriHeaderWD, apiUrl }
