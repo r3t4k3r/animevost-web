ca49f6c42990c0442fe7be03a42b7d9c944cd28b
foo <foo@example.com>
2024-10-18 11:20:18 +0300

    show log info in gh-pages.sh

--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -1,17 +1,17 @@
-var ghpages = require('gh-pages');
+var ghpages = require("gh-pages");
 
 ghpages.publish(
-    'public', // path to public directory
-    {
-        branch: 'gh-pages',
-        silent: true,
-        repo: 'https://github.com/r3t4k3r/animevost-web.git', // Update to point to your repository  
-        user: {
-            name: 'r3t4k3r', // update to use your name
-            email: 'av.19@list.ru' // Update to use your email
-        }
+  "public", // path to public directory
+  {
+    branch: "gh-pages",
+    silent: true,
+    repo: "https://github.com/r3t4k3r/animevost-web.git", // Update to point to your repository
+    user: {
+      name: "r3t4k3r", // update to use your name
+      email: "av.19@list.ru", // Update to use your email
     },
-    () => {
-        console.log('Deploy Complete!')
-    }
-)
+  },
+  (...foo) => {
+    console.log("Finished!", foo);
+  },
+);
